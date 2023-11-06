e304b229072a682a307a65ec70ef9c8652875b1a
vaclog <itrva@vaclog.com>
2023-11-06 14:19:07 -0300

    Create quadmind_status.bat

---
+++ quadmind_status.bat
@@ -0,0 +1,15 @@
+ECHO OFF
+FOR /F "tokens=*" %%A IN ('DATE/T') DO FOR %%B IN (%%A) DO SET Today=%%B
+
+FOR /F "tokens=1-3 delims=/-" %%A IN ("%Today%") DO (
+    SET Dia=%%A
+    SET Mes=%%B
+    SET Anio=%%C
+)
+
+SET FILENAMELOG=%Anio%-%Mes%-%Dia%
+echo %FILENAMELOG%
+ECHO OFF
+cd C:\Users\ValkUser\Documents\quadmind
+SET PY_EXE=python.exe
+%PY_EXE% C:\Users\ValkUser\Documents\quadmind\main_order_status.py >> c:\LOGS\quadmind-status-%FILENAMELOG%.txt 2>&1 
